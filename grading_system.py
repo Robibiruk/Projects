e021d72323a02b6507d2f363bc3943462a48eefe
Robibiruk <robekmedia@gmail.com>
2025-07-28 10:30:35 -0700

    Modified AAU grading System

--- grading_system.py
+++ grading_system.py
@@ -1,26 +1,33 @@
-#AAU grading system
-score = float(input("Enter your mark: "))
-if score >= 90:
-    grade = 'A+'
-elif score >= 85:
-    grade = 'A'
-elif score >= 80:
-    grade = 'A-'
-elif score >= 75:
-    grade = 'B+'
-elif score >= 68:
-    grade = 'B'
-elif score >= 65:
-    grade = 'B-'
-elif score >= 60:
-    grade = 'C+'
-elif score >= 50:
-    grade = 'C'
-elif score >= 45:
-    grade = 'C-'
-elif score >= 40:
-    grade = 'D'
-else:
-    grade = 'F'
-#print the results
-print(f"Your grade is: {grade}")
+    #AAU grading system
+while True: 
+
+    score = float(input("Enter your mark: "))
+    if score >= 90:
+        grade = 'A+'
+    elif score >= 85:
+        grade = 'A'
+    elif score >= 80:
+        grade = 'A-'
+    elif score >= 75:
+        grade = 'B+'
+    elif score >= 68:
+        grade = 'B'
+    elif score >= 65:
+        grade = 'B-'
+    elif score >= 60:
+        grade = 'C+'
+    elif score >= 50:
+        grade = 'C'
+    elif score >= 45:
+        grade = 'C-'
+    elif score >= 40:
+        grade = 'D'
+    else:
+        grade = 'F'
+    print(f"Your grade is: {grade}")
+    again = input("Try another? (yes/no): ").lower()
+    if again == "yes":
+        continue
+    else:
+        print("Goodbye!")
+        break
