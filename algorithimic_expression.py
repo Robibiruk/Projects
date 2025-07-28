d133f9ffed820c4b044b95f848a27a615f5087ac
Robibiruk <robekmedia@gmail.com>
2025-07-28 02:28:53 -0700

    mathematical algorithims

--- algorithimic_expression.py
+++ algorithimic_expression.py
@@ -3,6 +3,6 @@ b = 7 - 22       #subtruction
 c = 567 * 66     #multiplication
 d = 777 / 7      #division
 e = 286 % 12     #remainder
-
-print(a,b,c,d,e) #print results
+#printing results
+print(a,b,c,d,e) 
 
