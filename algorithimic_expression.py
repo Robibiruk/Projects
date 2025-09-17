dfc1d99ceed0495964e4973075bdf7ce727a1a96
Robibiruk <robekmedia@gmail.com>
2025-09-17 20:46:43 +0300

    Added

--- algorithimic_expression.py
+++ algorithimic_expression.py
@@ -1,8 +1,8 @@
-# a = 5 + 3        #addition
-# b = 7 - 22       #subtruction
-# c = 567 * 66     #multiplication
-# d = 777 / 7      #division
-# e = 286 % 12     #remainder
-# #printing results
-# print(a,b,c,d,e) 
+a = 5 + 3        #addition
+b = 7 - 22       #subtruction
+c = 567 * 66     #multiplication
+d = 777 / 7      #division
+e = 286 % 12     #remainder
+#printing results
+print(a,b,c,d,e) 
 
